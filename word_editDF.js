b90e3d88dbe0dfb2a6cb3b23b899d877062d16c3
gj9159a <124884721+gj9159a@users.noreply.github.com>
2024-06-05 23:04:32 +0500

    Update word_editDF.js

--- word_editDF.js
+++ word_editDF.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         word_editDF
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  редактирует все поля word...word9 в зоне видимости под МКБ-10
 // @author       gj9159a
 // @match        https://klientiks.ru/clientix/admin/dynamicfields
