24aaa989ff09b6106e9939c5716a313dce9b2cd4
gj9159a <124884721+gj9159a@users.noreply.github.com>
2024-06-20 18:16:55 +0500

    Update add_df_EGISZ.js

--- add_df_EGISZ.js
+++ add_df_EGISZ.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         add_df_EGISZ
 // @namespace    http://tampermonkey.net/
-// @version      1.1.2
+// @version      1.1.3
 // @description  добавляет динполя ЕГИСЗ в указанные протоколы, в карточку клиента и сотрудника. Также позволяет добавить документ "Протокол консультации (CDA) Редакция 4".
 // @author       gj9159a
 // @match        https://klientiks.ru/clientix/admin/dynamicfields
@@ -48,16 +48,16 @@
         addDoc1Button.style.display = 'none';
         document.querySelector("#DynamicFields > div.element-cr._label-left._inline").appendChild(addDoc1Button);
 
-        const remdOidInput = createInputField('Введите значение для {{account.remd_oid}}', '25%', '20%');
-        const medicalLicenseInput = createInputField('Введите значение для {{account.medical_license}}', '25%', '30%');
-        const medicalLicenseAuthorInput = createInputField('Введите значение для {{account.medical_license_author}}', '25%', '40%');
-        const addressCodeValueInput = createInputField('Введите значение для {{account.address_code_value}}', '25%', '50%');
-        const addressCodeKeyInput = createInputField('Введите значение для {{account.address_code_key}}', '25%', '60%');
+        const remdOidInput = createInputField('Введите значение для {{account.remd_oid}} Пример: 1.2.643.5.1.13.13.12.2.23.80641', '5%', '20%');
+        const medicalLicenseInput = createInputField('Введите значение для {{account.medical_license}} Пример: Л041-01126-23/00096607', '5%', '30%');
+        const medicalLicenseAuthorInput = createInputField('Введите значение для {{account.medical_license_author}} Пример: Министерство здравоохранения Краснодарского края. Дата регистрации: 31.03.2022', '5%', '40%');
+        const addressCodeValueInput = createInputField('Введите значение для {{account.address_code_value}} Пример: 23', '5%', '50%');
+        const addressCodeKeyInput = createInputField('Введите значение для {{account.address_code_key}} Пример: Краснодарский край', '5%', '60%');
 
         function createInputField(placeholder, right, top) {
             const input = document.createElement('textarea');
             input.rows = 4;
-            input.cols = 90;
+            input.cols = 140;
             input.placeholder = placeholder;
             input.style.position = 'absolute';
             input.style.right = right;
