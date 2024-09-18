b1fd306a936697c13e9723825b0392f0eec7d355
gj9159a <124884721+gj9159a@users.noreply.github.com>
2024-09-18 12:32:29 +0500

    Update add_df_EGISZ.js

--- add_df_EGISZ.js
+++ add_df_EGISZ.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         add_df_EGISZ
 // @namespace    http://tampermonkey.net/
-// @version      1.2.8
+// @version      1.2.9
 // @description  добавляет динполя ЕГИСЗ в указанные протоколы, в карточку клиента и сотрудника. Также позволяет добавить документ "Протокол консультации (CDA) Редакция 4".
 // @author       gj9159a
 // @match        https://klientiks.ru/clientix/admin/dynamicfields
